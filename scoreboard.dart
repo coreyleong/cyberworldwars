8c15fee4bb04379e4560f82ea77158340836e97a
Corey Leong <coreyleong@gmail.com>
2014-03-24 16:57:56 -0400

    0.3v commit

--- scoreboard.dart
+++ scoreboard.dart
@@ -15,6 +15,9 @@ class Scoreboard {
     height = canvas.height;
     clear();
     text();
+    playerlives();
+    pointsscored();
+    networkhealth();
     
   }
   
@@ -44,22 +47,33 @@ class Scoreboard {
     context.fillStyle = "black";
     context.font="10px Arial";
     context.fillText("Player Lives",10,10);
-    context.fillText("Points Scored",120,10);
-    context.fillText("Network Health",220,10);
+    context.fillText("Points Scored",110,10);
+    context.fillText("Network Health",210,10);
     
+  }
+  
+  playerlives() {
+  
     context.fillStyle = "green";
     context.font = "28px Arial";
     context.fillText("3",30,40); 
-    
+  }
+  
+  pointsscored() {
+   
     context.fillStyle = "blue";
     context.font = "28px Arial";
-    context.fillText("10,000",110,40); 
-    
-    context.fillStyle = "green";
-    context.font = "28px Arial";
-    context.fillText("Good", 221, 40);
-    
-    
+    context.fillText("0",135,40); 
   }
-
+  
+  networkhealth() {
+  
+    //context.font = "28px Arial";
+    
+  
+    context.fillStyle = "red";
+    context.font = "24px Arial";
+    context.fillText("Warning", 200, 40);
+    
+ }
 }
